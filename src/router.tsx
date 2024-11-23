56bdcb3fbe774185695505c12dbec6d0dc836bcf
vickydarlinn <uttapalsangwan@gmail.com>
2024-11-23 18:16:12 +0530

    feat:add fake data

--- src/router.tsx
+++ src/router.tsx
@@ -8,7 +8,7 @@ export const router = createBrowserRouter([
     element: <MainLayout />,
     children: [
       {
-        path: ".",
+        path: "",
         element: <HomePage />,
       },
     ],
